c85efdc4f08f8c56bc5848c60871b6494c8461d1
Daniel151505 <daniel150520011@gmail.com>
2022-09-25 16:35:27 -0500

    Objects

---
+++ objects.js
@@ -0,0 +1,14 @@
+miAuto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    annio: 2022,
+    detalleDelAuto: function() {
+        console.log(`Auto ${this.modelo} ${this.annio}`);
+    }
+};
+
+miAuto.detalleDelAuto();
+
+console.log(miAuto.marca);
+
+
