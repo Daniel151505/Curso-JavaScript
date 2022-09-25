d52a4e282e0f446a336e1d03cf1e1cb4a81d2620
Daniel151505 <daniel150520011@gmail.com>
2022-09-25 15:54:43 -0500

    Arrays

---
+++ arrays.js
@@ -0,0 +1,26 @@
+// Declarando arreglo
+var frutas = [
+    "Manzana", "Platano", "Cereza", "Fresa"
+];
+
+// Longitud
+console.log(frutad.length);
+
+// Posición del elemento
+console.log(frutas[0]);
+
+// Agregando elemento
+var masFrutas = frutas.push("Uvas");
+
+// Eliminando elemento
+var ultimo = frutas.pop("Uvas");
+
+// Agregando elemento al comienzo
+var nuevaLongitud = frutas.unshift("Pera");
+
+// Eliminando elemento del comienzo
+var borrarFruta = fruta.length.shift("Pera");
+
+// Posición del elemento
+var posicion = frutas.indexOf("Cereza");
+
