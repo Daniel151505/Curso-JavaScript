1a13f7cd705031304a3d1d4f71dd5d491a423e15
Daniel151505 <daniel150520011@gmail.com>
2022-09-25 16:27:08 -0500

    Agregando while

--- loops.js
+++ loops.js
@@ -9,3 +9,24 @@ for (i = 0; i < frutas.length; i++) {
 for (e of frutas) {
     console.log(`Elemento ${e}`);
 }
+
+//<-- while -->
+var estudiantes = ["Maria", "Sergio", "Rosa", "Daniel"];
+
+function saludarEstudiante(estudiante) {
+    console.log(`Hola ${estudiante}`);
+}
+
+var i = 0;
+
+// do-while
+do {
+    saludarEstudiante(estudiantes[i]);
+    i++;
+} while (i < estudiantes.length)
+
+// while
+while (estudiantes.length > 0) {
+    var estudiante = estudiantes.shift();
+    saludarEstudiante(estudiante);
+}
