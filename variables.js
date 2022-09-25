7cad90366fe15d760b1d1bc4bf0a254438383bd9
Daniel151505 <daniel150520011@gmail.com>
2022-09-25 12:24:51 -0500

    Variables

---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Daniel";
+
+// declarar
+var edad;
+
+// inicializar
+edad = 30;
+
+// arreglo
+var element = ["Computadora", "Celular"];
+
+// objeto
+var persona = {
+    nombre: "Sara",
+    edad: 30
+}
+
+// llamando
+persona
